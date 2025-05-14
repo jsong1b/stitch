bed8526c621053341a35f2592265c67c4ff6bac1
jsong1b <141759463+jsong1b@users.noreply.github.com>
2025-05-13 18:14:45 -0700

    Add block extraction to Pandoc frontend

--- stitch-pandoc.py
+++ stitch-pandoc.py
@@ -4,6 +4,7 @@
 import shutil
 import sys
 import os
+import json
 
 
 def main():
@@ -55,8 +56,123 @@ def main():
             file = sys.stderr
         )
         sys.exit(1)
+    blocks = []
+    for file in files:
+        unfiltered_blocks = {}
+        output = ""
+
+        try:
+            if file[0] == "auto":
+                output = os.popen(f"pandoc {file[1]} -t json").read()
+            else:
+                output = os.popen(f"pandoc -f {file[0]} {file[1]} -t json").read()
+
+            unfiltered_blocks = json.loads(output)['blocks']
+        except Exception as e:
+            print(
+                f"\033[33mPandoc Stitch Frontend: Warning: Could not run Pandoc on file \"{file[1]}\": {str(e)}",
+                file = sys.stderr
+            )
+            print(f"Skipping file \"{file[1]}\"...\033[0m", file = sys.stderr)
+            continue
+
+        blocks += extract_blocks(file[1], unfiltered_blocks)
 
 
+def extract_blocks(file, pandoc_blocks):
+    saved_blocks = []
+    cur_block = {}
+    block_metadata = None
+
+    for block in pandoc_blocks:
+        if type(block) == list:
+            saved_blocks += extract_blocks(file, block)
+        elif type(block) == dict:
+            if "t" not in block:
+                continue
+
+            if block_metadata != None:
+                if block["t"] != "CodeBlock":
+                    block_metadata = None
+                    continue
+
+                invalid_block = False
+                saved_block = {
+                    "lines":          block["c"][1].split("\n"),
+                    "from":           file,
+                    "export":         "",
+                    "name":           "",
+                    "append":         "",
+                    "append_to_from": "",
+                }
+                if block_metadata["t"] == "Link":
+                    saved_block["export"] = block_metadata["c"][2][0]
+
+                    for elem in block_metadata["c"][1]:
+                        if elem["t"] not in ["Str", "Space"]:
+                            invalid_block = True
+                            break
+
+                        if elem["t"] == "Space":
+                            saved_block["name"] += " "
+                        else:
+                            saved_block["name"] += elem["c"]
+                else:
+                    saved_block["name"] = block_metadata["c"][1]
+                if "+" in saved_block["name"]:
+                    if len(saved_block["name"].split("+")) > 2:
+                        invalid_block = True
+
+                    (
+                        saved_block["name"],
+                        saved_block["append"]
+                    ) = saved_block["name"].split("+")
+
+                if "@" in saved_block["append"]:
+                    if len(saved_block["append"].split("@")) > 2:
+                        invalid_block = True
+
+                    (
+                        saved_block["append"],
+                        saved_block["append_to_from"]
+                    ) = saved_block["append"].split("@")
+
+                if saved_block["export"] != "":
+                    if saved_block["name"] == "":
+                        saved_block["name"] == block_metadata["c"][2][0]
+
+                    saved_block["export"] = os.path.normpath(
+                        os.path.join(
+                            os.path.dirname(file),
+                            block_metadata["c"][2][0]
+                        )
+                    )
+                if invalid_block == True:
+                    block_metadata = None
+                    continue
+                cleaned_block = {}
+                for key, val in saved_block.items():
+                    if (type(val) == str and val == "" and
+                        key in ["export", "name", "append", "append_to_from"]):
+                        continue
+                    cleaned_block[key] = val
+
+                saved_blocks += [cleaned_block]
+
+            if block["t"] == "Para":
+                if not ((len(block["c"]) == 2)
+                    and (block["c"][0]["t"] in ["Code", "Link"])
+                    and (block["c"][1] == {"t": "Str", "c": ":"})):
+                    continue
+
+                block_metadata = block["c"][0]
+                continue
+
+            for key, val in block.items():
+                if type(val) == list:
+                    saved_blocks += extract_blocks(file, val)
+
+    return saved_blocks
 
 
 if __name__ == "__main__":
